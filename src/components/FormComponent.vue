14ee5d544ad2b3fbf2863c849d10a548110a02f4
Daria Karpenko <dariakarpenko@MacBook-Air-de-Daria.local>
2024-06-06 11:42:05 +0200

    new form with information

---
+++ src/components/FormComponent.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  handlesubmit: {
+    type: Function,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <form @submit="props.handlesubmit">
+    <div class="form-group">
+      <label for="exampleFormControlInput1">Email address</label>
+      <input
+        type="email"
+        name="email"
+        class="form-control"
+        id="exampleFormControlInput1"
+        placeholder="name@example.com"
+      />
+    </div>
+    <div class="form-group">
+      <label for="exampleFormControlInput2">Title</label>
+      <input
+        type="text"
+        name="title"
+        class="form-control"
+        id="exampleFormControlInput2"
+        placeholder="title"
+      />
+    </div>
+    <div class="form-group">
+      <label for="exampleFormControlInput3">Auteur</label>
+      <input
+        type="text"
+        name="auteur"
+        class="form-control"
+        id="exampleFormControlInput3"
+        placeholder="auteur"
+      />
+    </div>
+    <div class="form-group">
+      <label for="exampleFormControlTextarea1">Contenue</label>
+      <textarea
+        class="form-control"
+        name="contenue"
+        id="exampleFormControlTextarea1"
+        rows="3"
+      ></textarea>
+    </div>
+    <div id="example">
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </form>
+</template>
+
+<style scoped></style>
---
+++ src/components/Main.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from "vue";
+import FormComponent from "./FormComponent.vue";
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const data = Object.fromEntries(formData.entries());
+  console.log("Form Data:", data);
+};
+
+const example = ref({
+  name: "Vue.js",
+  greet(event) {
+    alert("slick");
+
+    if (event) {
+      alert("pas de click");
+    }
+  },
+});
+</script>
+
+<template>
+  <p>EVENTS LIST</p>
+  <FormComponent :handlesubmit="handleSubmit"></FormComponent>
+</template>
+
+<style scoped></style>
